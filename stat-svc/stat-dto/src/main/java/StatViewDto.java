1b630d5edba0f6534d75a809bda05190c4f12ffd
Max Kostyuk <kostyuk.max@gmail.com>
2023-06-02 13:46:36 +0600

    feat: добавлены модуль stat-svc и внутри него stat-dto; созданы dto классы для сохранения и получения статистики

---
+++ stat-svc/stat-dto/src/main/java/StatViewDto.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class StatViewDto {
+    private String app;
+    private String uri;
+    private Long hits;
+}
