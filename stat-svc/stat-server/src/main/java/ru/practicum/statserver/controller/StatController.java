2ec667ed71fd14a8e4f93636f8f7262a68312401
Max Kostyuk <kostyuk.max@gmail.com>
2023-06-06 23:57:38 +0600

    feat: добавлено логгирование при запросах эндпоинтов сервера статистики

--- stat-svc/stat-server/src/main/java/ru/practicum/statserver/controller/StatController.java
+++ stat-svc/stat-server/src/main/java/ru/practicum/statserver/controller/StatController.java
@@ -1,6 +1,7 @@
 package ru.practicum.statserver.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import ru.practicum.statdto.StatViewDto;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class StatController {
@@ -20,7 +22,8 @@ public class StatController {
     @PostMapping("/hit")
     @ResponseStatus(HttpStatus.CREATED)
     public void saveStatistics(@RequestBody StatHitDto statHit) {
-        //надо ли логгировать?
+        log.info("Creating stat record for app={} and url={}, ip={}, timestamp={}",
+                statHit.getApp(), statHit.getUri(), statHit.getIp(), statHit.getTimestamp());
         service.saveStat(statHit);
     }
 
@@ -29,7 +32,7 @@ public class StatController {
                                            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                            @RequestParam(required = false) String[] uris,
                                            @RequestParam(required = false, defaultValue = "false") boolean unique) {
-        //логгируем?
+        log.info("Statistics request with start={}, end={}, uris={} and unique={}", start, end, uris, unique);
         return service.getStatistics(start, end, uris, unique);
     }
 }
