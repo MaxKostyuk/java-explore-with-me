3244564c46c40cb77b5cc6073919d94bd5ea3c7b
Max Kostyuk <kostyuk.max@gmail.com>
2023-06-07 20:29:54 +0600

    feat: добавлены модель Category и создан файл schema.sql

---
+++ main-svc/src/main/java/ru/practicum/mainsvc/category/model/Category.java
@@ -0,0 +1,20 @@
+package ru.practicum.mainsvc.category.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 50, unique = true)
+    private String name;
+}
