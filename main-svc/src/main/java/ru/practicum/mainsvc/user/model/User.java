3492a2453b34bb59e50340048e7b3b8aaa2f39e1
Max Kostyuk <kostyuk.max@gmail.com>
2023-06-09 18:25:49 +0600

    feat: добавлена модель User, создание соответствующей таблицы добавлено в schema.sql

---
+++ main-svc/src/main/java/ru/practicum/mainsvc/user/model/User.java
@@ -0,0 +1,22 @@
+package ru.practicum.mainsvc.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
