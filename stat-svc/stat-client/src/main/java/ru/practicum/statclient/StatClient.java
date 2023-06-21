c72bb8246f753abe5cd01bbd112711a8443e76c1
Max Kostyuk <kostyuk.max@gmail.com>
2023-06-21 07:08:48 +0600

    fix: исправлены ошибки StatClient

--- stat-svc/stat-client/src/main/java/ru/practicum/statclient/StatClient.java
+++ stat-svc/stat-client/src/main/java/ru/practicum/statclient/StatClient.java
@@ -1,6 +1,7 @@
 package ru.practicum.statclient;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -9,12 +10,16 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.statdto.StatHitDto;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 @Service
 public class StatClient extends BaseClient {
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     @Autowired
-    public StatClient(String serverUrl, RestTemplateBuilder builder) {
+    public StatClient(@Value("${stat-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder.uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
                         .requestFactory(HttpComponentsClientHttpRequestFactory::new).build()
@@ -27,11 +32,11 @@ public class StatClient extends BaseClient {
 
     public ResponseEntity<Object> getStats(LocalDateTime start, LocalDateTime end, String[] uris, boolean unique) {
         Map<String, Object> parameters = Map.of(
-                "start", start.toString(),
-                "end", end.toString(),
+                "start", start.format(formatter),
+                "end", end.format(formatter),
                 "uris", String.join(",", uris),
                 "unique", unique
         );
-        return get("/stats", parameters);
+        return get("/stats?start={start}&end={end}&uris={uris}&unique={unique}", parameters);
     }
 }
