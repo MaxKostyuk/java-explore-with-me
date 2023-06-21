615906108b106deaaddaaf4de49119a6c3e63d61
Max Kostyuk <kostyuk.max@gmail.com>
2023-06-22 03:57:02 +0600

    feat: создан репозиторий CompilationRepository

---
+++ main-svc/src/main/java/ru/practicum/mainsvc/compilation/repository/CompilationRepository.java
@@ -0,0 +1,7 @@
+package ru.practicum.mainsvc.compilation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.practicum.mainsvc.compilation.model.Compilation;
+
+public interface CompilationRepository extends JpaRepository<Compilation, Long> {
+}
